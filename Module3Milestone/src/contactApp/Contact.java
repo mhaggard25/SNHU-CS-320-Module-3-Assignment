daa30970eca59d7902b5f4b1812d41f91964a47c
Matt <mhaggard25@gmail.com>
2022-09-18 12:13:39 -0500

    Updated Contact Service and Contact classes
    Modified some of the coded for the project to make it fit the prompt better.

--- Module3Milestone/src/contactApp/Contact.java
+++ Module3Milestone/src/contactApp/Contact.java
@@ -8,8 +8,32 @@ public class Contact{
 	private String contactAddress;
 	
 	// constructor
-	public Contact() {
-		// TODO Auto-generated constructor stub
+	public Contact(String id, String fName, String lName, String pNumber, String conAddress) {
+		if (id == null || id.length() > 10) {
+			throw new IllegalArgumentException("Invalid contact ID");
+		}
+		
+		if (fName == null || fName.length() > 10) {
+			throw new IllegalArgumentException("Invalid first name");
+		}
+		
+		if (lName == null || lName.length() > 10) {
+			throw new IllegalArgumentException("Invalid last name");
+		}
+		
+		if (pNumber == null || pNumber.length() > 10 || pNumber.length() < 10) {
+			throw new IllegalArgumentException("Invalid phone number");
+		}
+		
+		if (conAddress == null || conAddress.length() > 30) {
+			throw new IllegalArgumentException("Invalid address");
+		}
+		
+		this.contactID = id;
+		this.firstName = fName;
+		this.lastName = lName;
+		this.phoneNumber = pNumber;
+		this.contactAddress = conAddress;
 	}
 	
 	// getters
@@ -17,12 +41,8 @@ public class Contact{
 		return this.contactID;
 	}
 	
-	public String getFirstName() {
-		return this.firstName;
-	}
-	
-	public String getLastName() {
-		return this.lastName;
+	public String getName() {
+		return this.firstName + " " + this.lastName;
 	}
 	
 	public String getPhoneNumber() {
@@ -35,22 +55,42 @@ public class Contact{
 	
 	// setters
 	public void setContactID(String newId) {
+		if (newId == null || newId.length() > 10) {
+			throw new IllegalArgumentException("Invalid contact ID");
+		}
+		
 		contactID = newId;
 	}
 	
 	public void setFirstName(String newFirstName) {
+		if (newFirstName == null || newFirstName.length() > 10) {
+			throw new IllegalArgumentException("Invalid first name");
+		}
+		
 		firstName = newFirstName;
 	}
 	
 	public void setLastName(String newLastName) {
+		if (newLastName == null || newLastName.length() > 10) {
+			throw new IllegalArgumentException("Invalid last name");
+		}
+		
 		lastName = newLastName;
 	}
 	
 	public void setPhoneNumber(String newPhoneNumber) {
+		if (newPhoneNumber == null || newPhoneNumber.length() > 10 || newPhoneNumber.length() < 10) {
+			throw new IllegalArgumentException("Invalid phone number");
+		}
+
 		phoneNumber = newPhoneNumber;
 	}
 	
 	public void setContactAddress(String newContactAddress) {
+		if (newContactAddress == null || newContactAddress.length() > 30) {
+			throw new IllegalArgumentException("Invalid address");
+		}
+		
 		contactAddress = newContactAddress;
 	}
 	
--- Module3Milestone/src/contactApp/ContactService.java
+++ Module3Milestone/src/contactApp/ContactService.java
@@ -1,40 +1,80 @@
 package contactApp;
 
+import java.util.Vector;
+
 public class ContactService {
 	
-	Contact myContact = new Contact();
+	// Create a vector to store contacts.
+	private Vector<Contact> myContacts = new Vector<Contact>();
 	
-	// Add contacts
-	public void addContact(String firstName, String lastName, String phoneNumber, String address) {
-		
-		// Get user input for the contact information.
-		
-		// Add contact information using the setters from the Contact class.
-		myContact.setFirstName(firstName);
-		myContact.setLastName(lastName);
-		myContact.setPhoneNumber(phoneNumber);
-		myContact.setContactAddress(address);
+	// Create an int counter to store the number of contacts.
+	private int numContacts = 0;
+	
+	// Getter for numContacts
+	public int GetNumContacts() {
+		return numContacts;
 	}
 	
-	// Remove contacts using the contact ID
-		public void removeContact(String ID) {
-		// This doesn't do anything yet.
+	// Getter for the list of contacts.
+	public Vector<Contact> getContactLIst(){
+		return myContacts;
 	}
 	
-	// Update contacts per ID (only update firstName, lastName, phoneNumber, contactAddress)
-		public void updateContact(String ID) {
-			// This doesn't do anything yet.
+	// Function to add a contact to the list.
+	public void addContact(String contactID, String fName, String lName, String phoneNumber, String contactAddress) {
+		// Create a new contact
+		Contact newContact = new Contact(contactID, fName, lName, phoneNumber, contactAddress);
+		
+		// Add the contact to the list of contacts.
+		myContacts.add(newContact);
+		
+		// Increment the number of contacts.
+		numContacts++;
+	}
+	
+	public void addContact(Contact contact) {
+		// Add the contact to the list of contacts.
+		myContacts.add(contact);
+		
+		// Increment the number of contacts.
+		numContacts++;
+	}
+	
+	// Function to add a contact to the list.
+	public void removeContact(String contactID) {
+		
+		if (contactID == null || contactID.length() > 10) {
+			throw new IllegalArgumentException("Invalid contact ID");
 		}
-	
-	// Display contact for debug purposes
-	public void displayContact() {
-		System.out.println(myContact.getContactID() + ": " + myContact.getFirstName() + "  " + myContact.getLastName() + " | " + myContact.getPhoneNumber() + " | " + myContact.getContactAddress());
+		
+		if (myContacts.isEmpty()) {
+			throw new IllegalArgumentException("There are no contacts saved.");
+		}
+		
+		int index = -1;
+		for (Contact c: myContacts) {
+			if (c.getContactID() == contactID) {
+				index = myContacts.indexOf(c);
+			}
+		}
+		
+		if (index == -1) {
+			System.out.println("Not found.");
+			return;
+		}
+		
+		else {
+			myContacts.remove(index);
+			numContacts--;
+			System.out.println("Removed.");
+		}
 	}
 	
-	// If contactID is greater than 10 characters or null throw error
-	// If firstName is greater than 10 characters or null throw error
-	// If lastName is greater than 10 characters or null throw error
-	// If phoneNumber is greater than 10 or less than 10 characters or null throw error
-	// If contact address is greater than 30 characters or null throw error
-
+	public void removeContact(Contact contact) {
+		myContacts.remove(contact);
+		numContacts --;
+	}
+	
+	// Update contact
+	
 }
